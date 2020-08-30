7b9d56d6e817edfeeb576fa9d98e4943f6891261
red-star25 <nakumdhruv123@gmail.com>
2020-08-30 11:11:25 +0530

    alan-ai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web"
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from "./styles"
+import wordsToNumbers from "words-to-numbers"
+
+const alanKey = "4dfb8f8fc12871bb900a56d427eacd492e956eca572e1d8b807a3e2338fdd0dc/stage"
 
 function App() {
+
+  const classes = useStyles()
+
+  const [newsArticles,setNewsArticles] = useState([])
+  const [activeArticle,setActiveArticle]=useState(-1)
+
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand:({command,articles,number})=>{
+        if(command=== 'newHeadlines'){
+          setNewsArticles(articles);
+        }else if(command === "hightlight"){
+            setActiveArticle((prevActiveArticle)=>prevActiveArticle+1)
+        }else if(command ==="open"){
+          const parsedNumber = number.length >2 ?wordsToNumbers(number,{fuzzy:true}):number
+          const article = articles[parsedNumber-1];
+
+          if(parsedNumber >20){
+            alanBtn().playText("Please try that again")
+          }else if(article){
+            window.open(articles[parsedNumber].url,'_blank');
+            alanBtn().playText("Opening...")
+            
+          }
+        }else if(command === "changeTheme"){
+          if(document.body.style.backgroundColor==="black")
+          document.body.style ="background:white;"
+          else
+          document.body.style ="background:black;"
+        }
+      }
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.logoContainer}>
+        <img src="https://pbs.twimg.com/profile_images/697158410425495556/0fjHbvX4.jpg" className={classes.alanLogo} alt="alan logo"></img>
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </div>
   );
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,54 @@
+import React, { useEffect,useState,createRef } from 'react'
+import {Card,CardActions,CardActionArea, CardContent,CardMedia, Button , Typography} from "@material-ui/core"
+import useStyles from "./styles"
+import classNames from "classnames"
+
+const NewsCard = ({article:{description,publishedAt,source,title,url,urlToImage},i,activeArticle}) => {
+
+    const [elRefs,setElRefs] = useState([])
+    const classes = useStyles()
+    const scrollToRef = (ref)=> window.scroll(0,ref.current.offsetTop-50)
+
+    useEffect(()=>{
+        setElRefs((refs)=>Array(20).fill().map((_,j)=>refs[j]||createRef()))
+    },[])
+
+    useEffect(()=>{
+        if(i===activeArticle && elRefs[activeArticle]){
+            scrollToRef(elRefs[activeArticle])
+        }
+    },[i,activeArticle,elRefs])
+    return (
+        <Card ref={elRefs[i]} className={classNames(classes.card,activeArticle===i ?classes.activeCard:null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage||"https://www.itsmfusa.org/resource/resmgr/images/more_images/news-3.jpg"}/>
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">
+                        {(new Date(publishedAt)).toDateString()}
+                    </Typography>
+                    
+                    <Typography variant="body2" color="textSecondary" component="h2">
+                        {source.name}
+                    </Typography>
+                </div>
+                <Typography className={classes.title} glutterBottom varient="h5">
+                    {title}
+                </Typography>
+                <CardContent>
+                    <Typography varient="body2" color="textSecondary" component="p">
+                    {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More 
+                    <Typography varient="h5" color="textSecondary">
+                        {i+1}
+                    </Typography>
+                </Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default NewsCard
